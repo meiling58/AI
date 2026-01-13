e5c2922767c2056c07fcf1b4cf4817246596b0ac
Mei-Ling Liang <meiling.liang58@gmail.com>
2026-01-13 10:19:39 -0500

    add function to common

--- aiApi/common.py
+++ aiApi/common.py
@@ -23,3 +23,16 @@ def get_env_key(name):
     except KeyError:
         print("Error: API_KEY environment variable not set.")
     return api_key
+
+
+def get_api_key(name, source='file'):
+    key_sources = {'file', 'env'}
+    key = None
+    if source in key_sources:
+        if source == 'file':
+            key = get_file_content(name)
+        elif source == 'env':
+            key = get_env_key(name)
+    else:
+        print(f"Error: {source} not a Valid sources, please use file or env")
+    return key
